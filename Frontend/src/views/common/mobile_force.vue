f57cc6c585d04f177f2aa67377d2d869c76574a0
INMD1 <lyw514549@gmail.com>
2023-01-19 10:01:02 +0900

    업데이트

---
+++ Frontend/src/views/common/mobile_force.vue
@@ -0,0 +1,16 @@
+<template>
+    <div style="display: table;">
+        <!--이건 더미다-->
+        <v-col></v-col>
+        <v-col class="force_frame">
+            
+        </v-col>
+        <!--아도 몰라 이거 있어야 될거 같해-->
+        <div style="height: 8vh;"></div>
+    </div>
+</template>
+<script>
+export default {
+
+}
+</script>
