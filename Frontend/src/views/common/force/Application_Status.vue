9d03aab323defd5ffa0db75668502605b38f3655
INMD1 <87979171+INMD1@users.noreply.github.com>
2023-04-30 19:23:23 +0900

    2 컴포먼트 디자인 일부 완료

--- Frontend/src/views/common/force/Application_Status.vue
+++ Frontend/src/views/common/force/Application_Status.vue
@@ -8,47 +8,32 @@
         여기선 대대 전체 신청 진행 사항을 볼수있습니다.<br />
         다른 포대(중대) 용사들이 해당 포대 간부님게 문의 바람니다.
       </p>
-      <v-card>
-        <!--검색바-->
-        <v-toolbar flat>
+      <v-data-table
+        :headers="headers"
+        :items="sample"
+        item-key="name"
+        class="elevation-1"
+        :search="search"
+        :custom-filter="filterOnlyCapsText"
+      >
+        <template v-slot:top>
           <v-text-field
-            v-model="search_temp"
-            @input="search()"
-            style="width: 75vw; margin-top: 2vh"
+            v-model="search"
+            label="Search (UPPER CASE ONLY)"
+            class="mx-4"
           ></v-text-field>
-          <v-spacer></v-spacer>
-          <v-btn icon>
-            <v-icon>mdi-magnify</v-icon>
-          </v-btn>
-        </v-toolbar>
-        <!--데이터 테이블-->
-        <v-card v-for="item in temp" v-bind:key="item" class="mx-auto" max-width="344" outlined>
-          <v-list-item three-line>
-            <v-list-item-content>
-              <div class="text-overline mb-4">ID:ass1jks</div>
-              <v-list-item-title class="text-h5 mb-1">
-                {{item.Classes + item.name}}
-              </v-list-item-title>
-              <v-list-item-subtitle>이동장소: {{item.local}}</v-list-item-subtitle>
-              <v-list-item-subtitle
-                >신청시각: {{item.time}}</v-list-item-subtitle
-              >
-              <v-list-item-subtitle>승인여부: 대기중</v-list-item-subtitle>
-            </v-list-item-content>
-          </v-list-item>
-        </v-card>
-        <div style="height: 5vh"></div>
-      </v-card>
+        </template>
+      </v-data-table>
     </v-col>
   </div>
 </template>
+
 <script>
 export default {
   data() {
     return {
-      search_temp: "",
-      items: ["찬성", "반대", "보류"],
-      //임시로 데이터이다.
+      search: "",
+      calories: "",
       sample: [
         {
           Classes: "일병",
@@ -60,41 +45,55 @@ export default {
         {
           Classes: "일병",
           name: "왜무개",
-          calories: 237,
+          local: "어딘가가",
           time: "0000-00-00 00:00:00",
           verdict: "거절",
         },
         {
           Classes: "일병",
           name: "삼성갤력",
-          calories: 159,
+          local: "어딘가가",
           time: "0000-00-00 00:00:00",
           verdict: "거절",
         },
         {
           Classes: "일병",
           name: "삼그맘삼금",
-          calories: 305,
+          local: "어딘가가",
           time: "0000-00-00 00:00:00",
           verdict: "승인",
         },
       ],
-      //검색결과 데이터 임시저
-      temp: []
     };
   },
-  methods: {
-    search() {
-      this.temp = []
-        this.sample.forEach((item) => {
-          if (item.name.indexOf(this.search_temp) !== -1) {
-           this.temp.push(item)
-          }
-        });
+  computed: {
+    headers() {
+      return [
+        {
+          text: "계급",
+          align: "start",
+          sortable: false,
+          value: "Classes",
+        },
+        {
+          text: "이름",
+          value: "name",
+        },
+        { text: "장소", value: "local" },
+        { text: "시간", value: "time" },
+        { text: "승인여부", value: "verdict" },
+      ];
     },
   },
-  mounted() {
-    this.temp = this.sample
-  }
+  methods: {
+    filterOnlyCapsText(value, search) {
+      return (
+        value != null &&
+        search != null &&
+        typeof value === "string" &&
+        value.toString().toLocaleUpperCase().indexOf(search) !== -1
+      );
+    },
+  },
 };
 </script>
--- Frontend/src/views/common/force/Application_record.vue
+++ Frontend/src/views/common/force/Application_record.vue
@@ -8,112 +8,113 @@
           자신의 포대(중대)에 용사들의 이동 승인을 결정해주는 부분입니다.
         </p>
         <v-card>
-          <!--검색바-->
-          <v-toolbar flat>
-              <v-text-field
-                :rules="rules"
-                style="width: 75vw; margin-top: 2vh;"
-              ></v-text-field>
-            <v-spacer></v-spacer>
-            <v-btn icon>
-              <v-icon>mdi-magnify</v-icon>
-            </v-btn>
-          </v-toolbar>
-          <!--데이터 테이블-->
-          <v-card class="mx-auto" max-width="344" outlined>
-            <v-list-item three-line>
-              <v-list-item-content>
-                <div class="text-overline mb-4">ID:ass1jks</div>
-                <v-list-item-title class="text-h5 mb-1">
-                  상병 아무개
-                </v-list-item-title>
-                <v-list-item-subtitle>이동장소: 어딘가</v-list-item-subtitle>
-                <v-list-item-subtitle
-                  >신청시각: 0000-00-00 00:00:00</v-list-item-subtitle
-                >
-                <v-list-item-subtitle>승인여부: 대기중</v-list-item-subtitle>
-              </v-list-item-content>
-            </v-list-item>
-          <!--이 밑에 div 묶어 있는건 간부만 볼수 있습-->
-            <div style="display: flex">
-              <v-card-actions>
-                <v-btn outlined rounded text> 저장 </v-btn>
-                <v-select
-                  v-model="variant"
-                  :items="items"
-                  clearable
-                  label="결과"
-                  style="margin-left: 30vw; width: 25vw"
-                ></v-select>
-              </v-card-actions>
-            </div>
-          </v-card>
-          <v-card class="mx-auto" max-width="344" outlined>
-            <v-list-item three-line>
-              <v-list-item-content>
-                <div class="text-overline mb-4">ID:ass1jks</div>
-                <v-list-item-title class="text-h5 mb-1">
-                  상병 아무개
-                </v-list-item-title>
-                <v-list-item-subtitle>이동장소: 어딘가</v-list-item-subtitle>
-                <v-list-item-subtitle
-                  >신청시각: 0000-00-00 00:00:00</v-list-item-subtitle
-                >
-                <v-list-item-subtitle>승인여부: 대기중</v-list-item-subtitle>
-              </v-list-item-content>
-            </v-list-item>
+        <!--검색바-->
+        <v-toolbar flat>
+          <v-text-field
+            v-model="search_temp"
+            @input="search()"
+            style="width: 75vw; margin-top: 2vh"
+          ></v-text-field>
+          <v-spacer></v-spacer>
+          <v-btn icon>
+            <v-icon>mdi-magnify</v-icon>
+          </v-btn>
+        </v-toolbar>
+        <!--데이터 테이블-->
+        <v-card
+          v-for="item in temp"
+          v-bind:key="item"
+          class="mx-auto"
+          max-width="344"
+          outlined
+        >
+          <v-list-item three-line>
+            <v-list-item-content>
+              <div class="text-overline mb-4">ID:ass1jks</div>
+              <v-list-item-title class="text-h5 mb-1">
+                {{ item.Classes + item.name }}
+              </v-list-item-title>
+              <v-list-item-subtitle
+                >이동장소: {{ item.local }}</v-list-item-subtitle
+              >
+              <v-list-item-subtitle
+                >신청시각: {{ item.time }}</v-list-item-subtitle
+              >
+              <v-list-item-subtitle>승인여부: 대기중</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
           <!--이 밑에 div 묶어 있는건 간부만 볼수 있습-->
           <div style="display: flex">
-              <v-card-actions>
-                <v-btn outlined rounded text> 저장 </v-btn>
-                <v-select
-                  v-model="variant"
-                  :items="items"
-                  clearable
-                  label="결과"
-                  style="margin-left: 30vw; width: 25vw"
-                ></v-select>
-              </v-card-actions>
-            </div>
-          </v-card>
-          <div style="height: 5vh"></div>
+            <v-card-actions>
+              <v-btn outlined rounded text> 저장 </v-btn>
+              <v-select
+                v-model="variant"
+                :items="items"
+                clearable
+                label="결과"
+                style="margin-left: 30vw; width: 25vw"
+              ></v-select>
+            </v-card-actions>
+          </div>
         </v-card>
-      </v-col>
-    </div>
-  </template>
-  <script>
-  export default {
-    data() {
-      return {
-        items: ["찬성", "반대", "보류"],
-        desserts: [
-          {
-            name: "Frozen Yogurt",
-            calories: 159,
-            fat: 6.0,
-            carbs: 24,
-          },
-          {
-            name: "Ice cream sandwich",
-            calories: 237,
-            fat: 9.0,
-            carbs: 37,
-          },
-          {
-            name: "Eclair",
-            calories: 262,
-            fat: 16.0,
-            carbs: 23,
-          },
-          {
-            name: "Cupcake",
-            calories: 305,
-            fat: 3.7,
-            carbs: 67,
-          },
-        ],
-      };
+        <div style="height: 5vh"></div>
+      </v-card>
+    </v-col>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      search_temp: "",
+      items: ["찬성", "반대", "보류"],
+      //임시로 데이터이다.
+      sample: [
+        {
+          Classes: "일병",
+          name: "아무개",
+          local: "어딘가가",
+          time: "0000-00-00 00:00:00",
+          verdict: "거절",
+        },
+        {
+          Classes: "일병",
+          name: "왜무개",
+          calories: 237,
+          time: "0000-00-00 00:00:00",
+          verdict: "거절",
+        },
+        {
+          Classes: "일병",
+          name: "삼성갤력",
+          calories: 159,
+          time: "0000-00-00 00:00:00",
+          verdict: "거절",
+        },
+        {
+          Classes: "일병",
+          name: "삼그맘삼금",
+          calories: 305,
+          time: "0000-00-00 00:00:00",
+          verdict: "승인",
+        },
+      ],
+      //검색결과 데이터 임시저
+      temp: [],
+    };
+  },
+  methods: {
+    search() {
+      this.temp = [];
+      this.sample.forEach((item) => {
+        if (item.name.indexOf(this.search_temp) !== -1) {
+          this.temp.push(item);
+        }
+      });
     },
-  };
-  </script>
-  
+  },
+  mounted() {
+    this.temp = this.sample;
+  },
+};
+</script>
