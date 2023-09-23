b3f0fb1712629a0ae0ac3d1fcbb1a681337c14e6
INMD1 <87979171+INMD1@users.noreply.github.com>
2023-09-24 00:52:18 +0900

    백엔드 업데이트

--- Backend/config/server.js
+++ Backend/config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env("PUBLIC_URL", "https://strapi_test.powerinmd.com"),
+  // url: env("PUBLIC_URL", "https://strapi_test.powerinmd.com"),
   app: {
     keys: env.array('APP_KEYS'),
   },
